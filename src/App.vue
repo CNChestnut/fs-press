0c32bd16ba1078f21f24d9ae2d5d4f72990348cf
Chestnut Li <cn_l_chestnut@163.com>
2023-12-30 23:44:05 +0800

    a example
    一个渲染测试

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>Hello,world!</p>
+  <mdui-top-app-bar style="position: relative;">
+    <mdui-button-icon icon="menu"></mdui-button-icon>
+    <mdui-top-app-bar-title>Title</mdui-top-app-bar-title>
+    <div style="flex-grow: 1"></div>
+    <mdui-button-icon icon="more_vert"></mdui-button-icon>
+  </mdui-top-app-bar>
+  <div class="mdui-prose">
+    <div v-html="markdown_html"></div>
+  </div>
 </template>
+
+<script setup>
+
+import markdownIt from 'markdown-it'
+
+const md = new markdownIt()
+import { ref } from 'vue'
+
+var markdown_html = ref('ss')
+
+var markdown_text=
+`
+# 你好
+
+**这是一段粗体文字**
+`
+
+markdown_html.value = md.render(markdown_text)
+
+</script>
