76db984882360d02ce571d99173e38ae050333fa
Chestnut Li <cn_l_chestnut@163.com>
2023-12-30 22:54:50 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'mdui'
+import 'mdui/mdui.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
